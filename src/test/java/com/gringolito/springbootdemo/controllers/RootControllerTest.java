0b8e76c4fe68b811c36a1429de0b9ceb4762bc2c
Filipe Utzig <filipeutzig@gmail.com>
2020-04-17 20:08:46 -0300

    Add unit tests to RootController
    Signed-off-by: Filipe Utzig <filipeutzig@gmail.com>

---
+++ src/test/java/com/gringolito/springbootdemo/controllers/RootControllerTest.java
@@ -0,0 +1,35 @@
+package com.gringolito.springbootdemo.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(RootController.class)
+public class RootControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Test
+    public void getInfo() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("name").value(appName))
+                .andExpect(jsonPath("version").value(appVersion));
+    }
+}
