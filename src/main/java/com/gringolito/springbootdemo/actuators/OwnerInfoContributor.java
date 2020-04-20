b1ea7141fd5eef65be6363d90e65177810115590
Filipe Utzig <filipeutzig@gmail.com>
2020-04-19 21:34:17 -0300

    Add owner information on Actuator Info Endpoint
    This commit adds app owner, contact and GitHub project repository to the
    Actuator's Info Endpoint.
    
    Signed-off-by: Filipe Utzig <filipeutzig@gmail.com>

---
+++ src/main/java/com/gringolito/springbootdemo/actuators/OwnerInfoContributor.java
@@ -0,0 +1,25 @@
+package com.gringolito.springbootdemo.actuators;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OwnerInfoContributor implements InfoContributor{
+    @Value("${app.owner}")
+    private String projectOwner;
+
+    @Value("${app.repository}")
+    private String githubRepository;
+
+    @Value("${app.contact.email}")
+    private String contactEmail;
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("project_owner", projectOwner)
+               .withDetail("contact", contactEmail)
+               .withDetail("github_repository", githubRepository);
+    }
+}
