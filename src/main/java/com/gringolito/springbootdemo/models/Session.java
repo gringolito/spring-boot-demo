5e40298688480e0fe8168af16dad909d9bbf7952
Filipe Utzig <filipeutzig@gmail.com>
2020-04-20 20:43:15 -0300

    Use lombok to remove boilerplate code
    Signed-off-by: Filipe Utzig <filipeutzig@gmail.com>

--- src/main/java/com/gringolito/springbootdemo/models/Session.java
+++ src/main/java/com/gringolito/springbootdemo/models/Session.java
@@ -1,11 +1,15 @@
 package com.gringolito.springbootdemo.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
 @Entity(name = "sessions")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Session {
@@ -25,47 +29,4 @@ public class Session {
             joinColumns = @JoinColumn(name = "session_id"),
             inverseJoinColumns = @JoinColumn(name = "speaker_id"))
     private List<Speaker> speakers;
-
-    public Session() {
-    }
-
-    public Long getSession_id() {
-        return session_id;
-    }
-
-    public void setSession_id(Long session_id) {
-        this.session_id = session_id;
-    }
-
-    public String getSession_name() {
-        return session_name;
-    }
-
-    public void setSession_name(String session_name) {
-        this.session_name = session_name;
-    }
-
-    public String getSession_description() {
-        return session_description;
-    }
-
-    public void setSession_description(String session_description) {
-        this.session_description = session_description;
-    }
-
-    public Integer getSession_length() {
-        return session_length;
-    }
-
-    public void setSession_length(Integer session_length) {
-        this.session_length = session_length;
-    }
-
-    public List<Speaker> getSpeakers() {
-        return speakers;
-    }
-
-    public void setSpeakers(List<Speaker> speakers) {
-        this.speakers = speakers;
-    }
 }
